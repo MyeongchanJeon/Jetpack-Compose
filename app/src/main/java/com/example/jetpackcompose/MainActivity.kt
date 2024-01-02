46dc3d7847a3988ad78666d3bf78df208767dfbd
DESKTOP-21KBCK9\JMC <phy2685@gmail.com>
2024-01-02 23:09:55 +0900

    Top AppBar

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.jetpackcompose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
@@ -19,8 +22,8 @@ class MainActivity : ComponentActivity() {
             JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.padding(),
+                    color = MaterialTheme.colors.background,
                 ) {
                     //실제 화면
                     Greeting("")
@@ -30,9 +33,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private fun onClick(context: Context) {
+    Toast.makeText(
+        context,
+        "Item name",
+        Toast.LENGTH_SHORT
+    ).show()
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, context: Context = LocalContext.current) {
+    Scaffold(topBar = {
+        TopAppBar(title = {Text("TopAppBar")}
+        , backgroundColor = Color(0xff589ac6))
+    }) {
+        Text(text = "안녕하세요? $name")
+    }
 }
 
 @Preview(showBackground = true)
